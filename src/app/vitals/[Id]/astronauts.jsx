4c4cce430ed9f5939bfb8970b33799d1f5505907
Lucas Chen <130620394+lucasch37@users.noreply.github.com>
2024-09-08 10:05:51 -0700

    fixed more stuff

--- src/app/vitals/[Id]/astronauts.jsx
+++ src/app/vitals/[Id]/astronauts.jsx
@@ -13,30 +13,32 @@ import {
 const Astronauts = ({ journals }) => {
     const [status, setStatus] = useState(null);
     const handleReload = async () => {
-        setStatus(await getAstronautHealth(journals.at(-1).content));
+        setStatus(await getAstronautHealth(journals?.at(-1).content));
     };
     return (
         <div className={"flex flex-1 flex-row h-full mx-auto"}>
             <div className="-mt-2">
                 {status && <AstronautHealthChart status={status} />}
-                <div className="-mt-4 ">
-                    <TooltipProvider>
-                        <Tooltip>
-                            <TooltipTrigger asChild>
-                                <p className="text-xs text-center max-w-sm truncate border p-2 rounded-md">
-                                    {status?.justifications ||
-                                        "No data available"}
-                                </p>
-                            </TooltipTrigger>
-                            <TooltipContent>
-                                <p>
-                                    {status?.justifications ||
-                                        "No data available"}
-                                </p>
-                            </TooltipContent>
-                        </Tooltip>
-                    </TooltipProvider>
-                </div>
+                {status && (
+                    <div className="-mt-4 ">
+                        <TooltipProvider>
+                            <Tooltip>
+                                <TooltipTrigger asChild>
+                                    <p className="text-xs text-center max-w-sm truncate border p-2 rounded-md">
+                                        {status?.justifications ||
+                                            "No data available"}
+                                    </p>
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                    <p>
+                                        {status?.justifications ||
+                                            "No data available"}
+                                    </p>
+                                </TooltipContent>
+                            </Tooltip>
+                        </TooltipProvider>
+                    </div>
+                )}
             </div>
             <button
                 className="w-fit h-fit absolute top-4 right-4 bg-zinc-800 hover:bg-zinc-700 text-white transition ease-in-out duration-300 px-4 py-2 rounded-md"
